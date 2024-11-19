436973e1d9580bbd6da362c316d8b1b2e0e7dcd8
Roshanshres <Roshanshrestha992@gmail.com>
2024-11-19 22:03:47 +0545

    carts is added

--- store/models.py
+++ store/models.py
@@ -32,7 +32,18 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
 
+    @property
+    def get_cart_item(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
@@ -40,8 +51,15 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default = 0, null=True, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
-    # def __str__(self):
-    #     return str(self.id)
+    def __str__(self):
+        return str(self.id)
+    
+    @property 
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
+    
 
 class ShippingAddress(models.Model):
     Customer = models.ForeignKey(Customer, on_delete= models.SET_NULL, null= True,  blank=True,)
@@ -55,3 +73,5 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return self.address
+    
+
--- store/views.py
+++ store/views.py
@@ -8,9 +8,19 @@ def store(request):
 
 
 def cart(request):
-    context = {}
-    return render(request, 'store/cart.html')
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer= customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        #create empty cart for now for none0logged in users
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
     context = {}
-    return render(request, 'store/checkout.html')
+    return render(request, 'store/checkout.html', context)
