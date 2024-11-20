3d7101d1a178ab1d00e929508e477ed6ab8480c2
Roshanshres <Roshanshrestha992@gmail.com>
2024-11-20 22:27:22 +0545

    update user order in cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -12,8 +12,28 @@ for (i = 0; i < updateBtns.length; i++) {
             console.log('User is not authenticated')
                     
         }else{
-            console.log('User is authenticated, sending data...')
+            updateUserOrder(productId, action)
         }
-                
-	})
+    })
 }
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+
+		var url = '/update_item/'
+
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    console.log('Data:', data)
+		});
+}
+        
+       
